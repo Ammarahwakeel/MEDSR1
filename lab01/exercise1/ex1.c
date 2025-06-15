3b24d83881bbbb4a1f5fc8d67b41e0a6f72bcb38
Ammarahwakeel <ammarahwakeel9@gmail.com>
2025-06-14 23:37:23 -0700

    Completed lab01 exercise1 and added test cases

--- lab01/exercise1/ex1.c
+++ lab01/exercise1/ex1.c
@@ -5,15 +5,45 @@
 There are two different ways to iterate through a string.
 1st way hint: strlen() may be useful
 2nd way hint: all strings end in a null terminator */
-int num_occurrences(char *str, char letter) {
-    /* TODO: implement num_occurances */
-    return 0;
+int num_occurrences(char *str, char letter) { // using 2 hint
+    int count = 0;
+    while (*(str) != '\0') { // MAKE THE LOOP RUN UNTILL THE END OF STRING (I.E: IT ENCOUNTER NULL TERMINATOR)
+        if (*(str) == letter) {  // IF THE STRING CHARACHTER EQUALS THE REQUIRED LETTER THEN INCREMENT COUNT. 
+            count++;
+        }
+        str++;  // move to next character
+    }
+    return count;
 }
+
+
+
 
 /* Populates DNA_SEQ with the number of times each nucleotide appears.
 Each sequence will end with a NULL terminator and will have up to 20 nucleotides.
 All letters will be upper case. */
 void compute_nucleotide_occurrences(DNA_sequence *dna_seq) {
-    /* TODO: implement compute_nucleotide_occurances */
+    // Initialize counts to zero
+    dna_seq->A_count = 0;
+    dna_seq->C_count = 0;
+    dna_seq->G_count = 0;
+    dna_seq->T_count = 0;
+
+    char *str = dna_seq->sequence;  // Get pointer to the DNA string
+
+    while (*(str) != '\0') {  // Loop until end of string (null terminator)
+        if (*(str) == 'A') {     // If character is 'A', increment A_count
+            dna_seq->A_count++;
+        } else if (*(str) == 'C') {  // If 'C', increment C_count
+            dna_seq->C_count++;
+        } else if (*(str) == 'G') {  // If 'G', increment G_count
+            dna_seq->G_count++;
+        } else if (*(str) == 'T') {  // If 'T', increment T_count
+            dna_seq->T_count++;
+        }
+        str++;  // Move to next character
+    }
     return;
 }
+
+   
--- lab01/exercise1/test_ex1.c
+++ lab01/exercise1/test_ex1.c
@@ -7,14 +7,20 @@ int main(int argc, char **argv) {
     printf("Running tests...\n\n");
     /************ Part 1 Test Cases ************/
     char *str = "hello world!";
+    char *empty_str = "";
 
-    int num_l = num_occurrences(str, 'l');
+
+    int num_l = num_occurrences(str, 'l'); //TEST CASE 1
     assert(num_l == 3);
 
-    int num_z = num_occurrences(str, 'z');
+    int num_z = num_occurrences(str, 'z'); // TEST CASE 2
     assert(num_z == 0);
 
+
     /* TODO: Think of a scenario that is not tested by the current test cases. Create one additional test case to test this scenario. */
+    
+    int num_empty = num_occurrences(empty_str, 'a');
+    assert(num_empty == 0);  // Expect 0 occurrences
 
     printf("Congrats! If you have made it to this line, your Part 1 Test cases are all passing!\n");
 
@@ -36,6 +42,15 @@ int main(int argc, char **argv) {
     assert(dna_seq_2.T_count == 0);
 
     /* TODO: Think of a scenario that is not tested by the current test cases. Create one additional test case to test this scenario. */
+
+    /* Additional test case: empty sequence */
+     DNA_sequence dna_seq_3;
+     strcpy(dna_seq_3.sequence, "");
+     compute_nucleotide_occurrences(&dna_seq_3);
+     assert(dna_seq_3.A_count == 0);
+     assert(dna_seq_3.C_count == 0);
+     assert(dna_seq_3.G_count == 0);
+     assert(dna_seq_3.T_count == 0);
 
     printf("Congrats! If you have made it to this line, your Part 2 Test cases are all passing!\n");
 
